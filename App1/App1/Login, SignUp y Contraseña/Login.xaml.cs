0be3d395425cc50e4fe061e7a16ff512a2b3c66e
Lenovo <Lenovo@DESKTOP-3FCM9TP>
2021-10-12 23:19:08 -0500

    Agregar archivos de proyecto.

---
+++ App1/App1/Login, SignUp y Contraseña/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void RestablecerContrasena(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RestablecerContraseña());
+        }
+
+        private async void Ingresar(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Ingresar());
+        }
+    }       
+}
---
+++ App1/App1/Login, SignUp y Contraseña/RestablecerContraseña.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecuperarContraseña : ContentPage
+    {
+        public RecuperarContraseña()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConfirmacionCambioContra(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Restablecer());
+        }
+    }
+}
